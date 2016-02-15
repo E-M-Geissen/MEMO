9e9daadf2634ccac9e73f110031d0e3e99956630
E-M-Geissen <geissen@ist.uni-stuttgart.de>
2016-02-15 10:11:41 +0100

    cleanup

--- MEMO/NGF-Erk_data_analysis/install_toolboxes.m
+++
@@ -1,8 +0,0 @@
-addpath(genpath('D:\modelling\matlab\MixtureModeling_Checkpoint_PNAS\auxiliary'));
-addpath(genpath('D:\modelling\matlab\Toolboxen\EstTool'));
-addpath(genpath('D:\modelling\matlab\MixtureModeling_Checkpoint_PNAS\NGF-Erk_for_pub\project'));
-addpath(genpath('D:\modelling\matlab\Toolboxen\mcmcstat'));
-% General properties
-TextSizes.DefaultAxesFontSize = 12;
-TextSizes.DefaultTextFontSize = 14;
-set(0,TextSizes);
